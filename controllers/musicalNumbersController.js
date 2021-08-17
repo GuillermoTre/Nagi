b95908274e33e82f14028ded89970c7649d931ab
Luis Guillermo <luisg.trevinoc@gmail.com>
2021-08-17 13:40:46 -0500

    Added func to get numbers

--- controllers/musicalNumbersController.js
+++ controllers/musicalNumbersController.js
@@ -1,5 +1,28 @@
-function returnNumbers(){
 
+
+function returnNumbers(req,res){
+    for(let i = 1; i < 101; i++){
+        const mult3 = i % 3;
+        const mult5 = i % 5;
+
+        if(mult5 == 0 && mult3 ==0){
+            console.log('Musical');
+            continue
+        }
+
+        switch(mult3){
+            case 0: console.log('Music'); break;
+            default:
+                switch(mult5){
+                    case 0 : console.log('TI'); break;
+                    default: console.log(i);
+                }
+            break;
+
+        }
+
+    }
+    return res.status(200).json('done');
 }
 
 module.exports = {returnNumbers};
